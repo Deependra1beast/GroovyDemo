3d8da53580f45992ce131fab4cb8f71e57d07b9c
Deependra1beast <65725128+Deependra1beast@users.noreply.github.com>
2022-07-05 05:53:46 +0200

    Update GradleScript.groovy

--- sharedLib/vars/GradleScript.groovy
+++ sharedLib/vars/GradleScript.groovy
@@ -2,7 +2,6 @@ import com.gradle.gradlebuild
 
 def call(){
     echo 'this is the Gradle project'
-    echo 'this is the Gradle project for ${name}'
     def gradlebuild =new gradlebuild(script:this)
     gradlebuild.gradle()
 }
